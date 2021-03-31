70f3701db2a23889d99c1360d1135b19f6f164d1
Ned Bingham <talon4196@gmail.com>
2021-03-30 20:53:57 -0400

    got the parser running

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
+#include <iostream>
+
 #include "order.h"
 #include "simulator.h"
 
-int main()
+int main(int argc, char **argv)
 {
+	if (argc > 1) {
+		pgen::order_t order;
+		pgen::grammar_t grammar;
+		order.load(grammar);
+
+		pgen::lexer_t lexer;
+		lexer.open(argv[1]);
+
+		pgen::parsing result = grammar.parse(lexer);
+		if (result.msgs.size() == 0) {
+			result.tree.emit(lexer);
+		} else {
+			for (auto msg : result.msgs) {
+				std::cout << msg << std::endl;
+			}
+		}
+	}
 }
 
--- src/order.h
+++ src/order.h
@@ -14,8 +14,8 @@ struct order_t
 	int32_t AGGREGATE;
 	int32_t CONSTRAINT;
 	int32_t TASK;
-	int32_t INSTANCE;
 	int32_t EXPRESSION;
+	int32_t INSTANCE;
 	int32_t BOOLEAN;
 	int32_t TEXT;
 	int32_t EFFECT;
@@ -23,7 +23,7 @@ struct order_t
 	int32_t BOOLEAN1;
 	int32_t EXPRESSION0;
 	int32_t EXPRESSION1;
-	int32_t FLOAT;
+	int32_t INTEGER;
 
 	void load(grammar_t &grammar);
 };

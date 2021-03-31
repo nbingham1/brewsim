f2b87ea9a303cdad5d10bcc7cd4b72f39fce2610
Ned Bingham <talon4196@gmail.com>
2021-03-31 19:38:48 -0400

    making the expression syntax more sane

--- src/order.h
+++ src/order.h
@@ -16,13 +16,14 @@ struct order_t
 	int32_t TASK;
 	int32_t EXPRESSION;
 	int32_t INSTANCE;
-	int32_t BOOLEAN;
 	int32_t TEXT;
 	int32_t EFFECT;
-	int32_t BOOLEAN0;
-	int32_t BOOLEAN1;
 	int32_t EXPRESSION0;
 	int32_t EXPRESSION1;
+	int32_t EXPRESSION2;
+	int32_t EXPRESSION3;
+	int32_t EXPRESSION4;
+	int32_t EXPRESSION5;
 	int32_t INTEGER;
 
 	void load(grammar_t &grammar);
